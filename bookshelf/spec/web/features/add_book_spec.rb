b5cbf5bd3320466afb750ade5c90dc7ff215911d
HaruGakkaP <ffdd270@gmail.com>
2019-11-08 15:40:30 +0900

    Fix Index Spec. Create Add Book Spec. New File for Add Book.

---
+++ bookshelf/spec/web/features/add_book_spec.rb
@@ -0,0 +1,22 @@
+require './spec/features_helper'
+
+RSpec.describe 'Add a book' do
+  after do
+    BookRepository.new.clear
+  end
+
+  it 'can create a new book' do
+    visit '/books/new'
+
+    # 폼에 내용을 입력.
+    within 'form#book-form' do
+      fill_in 'Title', with: 'New Book'
+      fill_in 'Author', with: 'Some author'
+
+      click_button 'Create'
+    end
+
+    expect(page).to have_current_path('/books')
+    expect(page).to have_content('New book')
+  end
+end
