2102542fc462be018ae62d2bd7ce10aac11f224a
HaruGakkaP <ffdd270@gmail.com>
2019-11-15 00:53:32 +0900

    Add 'How To Add Book on Page'

--- bookshelf/spec/web/features/add_book_spec.rb
+++ bookshelf/spec/web/features/add_book_spec.rb
@@ -10,13 +10,13 @@ RSpec.describe 'Add a book' do
 
     # 폼에 내용을 입력.
     within 'form#book-form' do
-      fill_in 'Title', with: 'New Book'
+      fill_in 'Title', with: 'My Book'
       fill_in 'Author', with: 'Some author'
 
       click_button 'Create'
     end
 
     expect(page).to have_current_path('/books')
-    expect(page).to have_content('New book')
+    expect(page).to have_content("My Book")
   end
 end
